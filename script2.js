2c53e98e058d9bc082f7ba970c56d582ca4fc360
adodoCoder <alavropulido@whccd.edu>
2024-10-23 19:59:32 -0700

    first commit

---
+++ script2.js
@@ -0,0 +1,30 @@
+let myVariable = "Hello World!";
+console.log(myVariable); // string variable
+
+//change the string variable value
+myVariable = "I changed the string variable here";
+console.log(myVariable);
+
+
+// const variable
+const pi = 3.14;
+pi = 2; // throws an error
+
+
+let myNumber = 3;
+console.log(myNumber);
+
+console.log(Math.min)
+
+let myGreeting = "Hello";
+console.log(myGreeting);
+
+const pi = 3.14;
+// p = 2; 
+
+let myNumber = 333;
+console.log(Math.min(myNumber, pi 1)); //3.14
+
+myGreeting = "Hi";
+console.log(myGreeting);
+
